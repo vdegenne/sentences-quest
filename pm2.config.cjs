c32de8828b696deb65654bf044a421c9ddbd8594
vdegenne <vdegenne@gmail.com>
2023-01-13 11:35:39 +0100

    1

---
+++ pm2.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  apps: [{
+    name: 'sentences-quest',
+    script: 'server.js',
+    watch: 'server.js'
+  }]
+}
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { LitElement, html, css } from 'lit'
+import { customElement } from 'lit/decorators.js'
+import '@material/mwc-snackbar'
+import '@material/mwc-button'
+// import '@material/mwc-icon-button'
+// import '@material/mwc-dialog'
+// import '@material/mwc-textfield'
+// import '@material/mwc-checkbox'
+
+declare global {
+  interface Window {
+    app: AppContainer;
+    toast: (labelText: string, timeoutMs?: number) => void;
+  }
+}
+
+@customElement('app-container')
+export class AppContainer extends LitElement {
+
+  static styles = css``
+
+  render () {
+    return html`
+    <mwc-button>Hello There ! The app content goes here.</mwc-button>
+    `
+  }
+}
